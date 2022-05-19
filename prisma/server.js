9941685a4880da4f5e587e35b49a2f4456ae7d2d
Haim Guel Quiroz <hgq_2000@hotmail.com>
2022-05-19 13:34:01 -0500

    Add endpoints to show explorers info

--- prisma/server.js
+++ prisma/server.js
@@ -14,3 +14,14 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Listening to requests on port ${port}`);
 });
+
+app.get('/explorers', async (req, res) => {
+    const allExplorers =  await prisma.explorer.findMany({});
+    res.json(allExplorers);
+  });
+
+  app.get('/explorers/:id', async (req, res) => {
+    const id = req.params.id;
+    const explorer = await prisma.explorer.findUnique({where: {id: parseInt(id)}});
+    res.json(explorer);
+  });
