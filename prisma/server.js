6e3a3712ba546f02ff5f1441ed482dec91541e73
Haim Guel Quiroz <hgq_2000@hotmail.com>
2022-06-06 23:06:34 -0500

    Fix Corse

--- prisma/server.js
+++ prisma/server.js
@@ -7,6 +7,15 @@ const port = process.env.PORT || 3000;
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
+//Cors
+const cors = require("cors");
+
+const corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
 app.get('/', (req, res) => {
   res.json({message: 'alive'});
 });
@@ -15,22 +24,23 @@ app.listen(port, () => {
   console.log(`Listening to requests on port ${port}`);
 });
 
-app.get('/explorers', async (req, res) => {
+//get All exploers
+  app.get('/explorers', async (req, res) => {
     const allExplorers =  await prisma.explorer.findMany({});
     res.json(allExplorers);
   });
 
-  app.get('/explorers/:id', async (req, res) => {
-    const id = req.params.id;
-    const explorer = await prisma.explorer.findUnique({where: {id: parseInt(id)}});
-    res.json(explorer);
-  });
+  // app.get('/explorers/:id', async (req, res) => {
+  //   const id = req.params.id;
+  //   const explorer = await prisma.explorer.findUnique({where: {id: parseInt(id)}});
+  //   res.json(explorer);
+  // });
 
   app.post('/explorers', async (req, res) => {
     const explorer = {
-      name: "Haim",
-      username: "hA1nn-dot",
-      mission: "Node"
+      name: req.body.name,
+      username: req.body.username,
+      mission: req.body.mission
      };
     const message = 'Explorer creado.';
     await prisma.explorer.create({data: explorer});
@@ -38,66 +48,67 @@ app.get('/explorers', async (req, res) => {
   });
 
   app.put('/explorers/:id', async (req, res) => {
-	const id = parseInt(req.params.id);
+    const ids = parseInt(req.params.id);
+  
+    await prisma.explorer.update({
+      where: {
+        id: ids
+      },
+      data: {
+        mission: req.body.mission
+      }
+    })
+  
+    return res.json({message: "Actualizado correctamente"});
+  });
 
-	await prisma.explorer.update({
-		where: {
-			id: id
-		},
-		data: {
-			mission: "Java"
-		}
-	})
+// app.delete('/explorers/:id', async (req, res) => {
+// 	const id = parseInt(req.params.id);
+// 	await prisma.explorer.delete({where: {id: id}});
+// 	return res.json({message: "Eliminado correctamente",id: id});
+// });
 
-	return res.json({message: "Actualizado correctamente"});
-});
+// app.get('/launchX', async (req, res) => {
+//   const allExplorers =  await prisma.LaunchX.findMany({});
+//   res.json(allExplorers);
+// });
 
-app.delete('/explorers/:id', async (req, res) => {
-	const id = parseInt(req.params.id);
-	await prisma.explorer.delete({where: {id: id}});
-	return res.json({message: "Eliminado correctamente",id: id});
-});
+// app.get('/launchX/:id', async (req, res) => {
+//   const id = req.params.id;
+//   const explorer = await prisma.LaunchX.findUnique({where: {id: parseInt(id)}});
+//   res.json(explorer);
+// });
 
-app.get('/launchX', async (req, res) => {
-  const allExplorers =  await prisma.LaunchX.findMany({});
-  res.json(allExplorers);
-});
+// app.post('/launchX', async (req, res) => {
+//   const explorer = {
+//     name: "Juan",
+//     lang: "Java",
+//     missionCommander: "FerOchoa",
+//     enrollments: 5
+//    };
+//   const message = 'Explorer creado.';
+//   await prisma.LaunchX.create({data: explorer});
+//   return res.json({message});
+// });
 
-app.get('/launchX/:id', async (req, res) => {
-  const id = req.params.id;
-  const explorer = await prisma.LaunchX.findUnique({where: {id: parseInt(id)}});
-  res.json(explorer);
-});
+// app.put('/launchX/:id', async (req, res) => {
+// 	const id = parseInt(req.params.id);
 
-app.post('/launchX', async (req, res) => {
-  const explorer = {
-    name: "Juan",
-    lang: "Java",
-    missionCommander: "FerOchoa",
-    enrollments: 5
-   };
-  const message = 'Explorer creado.';
-  await prisma.LaunchX.create({data: explorer});
-  return res.json({message});
-});
+// 	await prisma.LaunchX.update({
+// 		where: {
+// 			id: id
+// 		},
+// 		data: {
+// 			lang: "Java"
+// 		}
+// 	})
 
-app.put('/launchX/:id', async (req, res) => {
-	const id = parseInt(req.params.id);
+// 	return res.json({message: "Actualizado correctamente"});
+// });
 
-	await prisma.LaunchX.update({
-		where: {
-			id: id
-		},
-		data: {
-			lang: "Java"
-		}
-	})
+// app.delete('/launchX/:id', async (req, res) => {
+// 	const id = parseInt(req.params.id);
+// 	await prisma.LaunchX.delete({where: {id: id}});
+// 	return res.json({message: "Eliminado correctamente",id: id});
+// });
 
-	return res.json({message: "Actualizado correctamente"});
-});
-
-app.delete('/launchX/:id', async (req, res) => {
-	const id = parseInt(req.params.id);
-	await prisma.LaunchX.delete({where: {id: id}});
-	return res.json({message: "Eliminado correctamente",id: id});
-});
